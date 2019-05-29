463f5a9b71462392b53cfc4d7a37fc3d0babe912
danbui57 <danbui57@gmail.com>
2019-05-28 22:52:48 -0400

    removes statement functions from burger.js and adds input fields for database table

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -11,7 +11,7 @@ var express = require("express");
 
 var router = express.Router();
 
-// Import the model (burger.js) to use its database functions.
+// Import the model (burger.js) to use it's database functions.
 var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
--- models/burger.js
+++ models/burger.js
@@ -1,26 +1,19 @@
-// Inside `burger.js`, import `orm.js` into `burger.js`
-// * Also inside `burger.js`, create the code that will call the ORM functions using burger specific input for the ORM.
-
-//     * Export at the end of the `burger.js` file.
-
-var orm = require("../config/orm.js");
-
-var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
-      cb(res);
-    });
-  },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
-      cb(res);
-    });
-  },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+module.exports = (sequelize, DataTypes) => {
+  const Burger = sequelize.define("Burger", {
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false
+    }
+  }, {
+    classMethods: {
+      associate: (models) => {
+        Burger.hasOne(models.Customer);
+      }
+    }
+  });
+  return Burger;
 }
-module.exports = burger;
